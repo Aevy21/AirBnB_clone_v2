349223d9d01b67e80170d22dfa9886a282b006cb
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-03-07 13:42:34 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,76 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-This file contains a script that distribute an archive to my web servers
-using the function do_deploy
+This script generates a .tgz archive from the web_static folder
+and distributes it to web servers using Fabric.
 """
 
+from fabric import task, Connection, env, run
+from fabric.api import local
+from os.path import exists
+from datetime import datetime
 
-import os
-from fabric.api import put, run, env
-
-
+env.hosts = ['54.165.47.248', '100.25.193.107']
 env.user = 'ubuntu'
-env.hosts = ['34.204.95.239', '54.234.17.205']
 
+def do_pack():
+    """
+    Generates a .tgz archive from the web_static folder.
+
+    Returns:
+        str: Archive path if the archive has been correctly generated, otherwise None.
+    """
+    # Get current timestamp
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Define archive name with the specified format
+    archive_name = "web_static_{}.tgz".format(current_time)
+
+    # Define archive path
+    archive_path = "versions/{}".format(archive_name)
+
+    print("Packing web_static to {}".format(archive_path))
+
+    # Create versions folder if it doesn't exist
+    local("mkdir -p versions")
+
+    # Create tgz archive
+    result = local("tar -cvzf {} web_static".format(archive_path), capture=True)
+
+    # Check if archive was created successfully
+    if result.succeeded:
+        print("web_static packed: {} ".format(archive_path))
+        return archive_path
+    else:
+        print("Failed to pack web_static")
+        return None
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
-    if not os.path.exists(archive_path):
+    """
+    Distributes an archive to web servers.
+
+    Args:
+        archive_path (str): Path to the archive.
+
+    Returns:
+        bool: True if all operations have been done correctly, otherwise False.
+    """
+    if not exists(archive_path):
+        print("Archive doesn't exist.")
         return False
-    try:
-        # Extract the filename without extension
-        filename = os.path.basename(archive_path)
-        name, ext = os.path.splitext(filename)
 
-        # Define the release directory using the extracted name
-        release_dir = f"/data/web_static/releases/{name}/"
+    archive_filename = archive_path.split('/')[-1]
+    archive_folder = archive_filename.replace('.tgz', '')
 
-        # copy archive to server
-        put(archive_path, "/tmp/")
+    for host in env.hosts:
+        with Connection(host) as c:
+            c.put(archive_path, '/tmp')
 
-        # uncompress the archive to a folder
-        run(f"mkdir -p {release_dir}")
-        run(f"tar -xzf /tmp/{filename} -C {release_dir}")
+            c.run('mkdir -p /data/web_static/releases/{}'.format(archive_folder))
+            c.run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_filename, archive_folder))
+            c.run('rm /tmp/{}'.format(archive_filename))
+            c.run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_folder, archive_folder))
+            c.run('rm -rf /data/web_static/releases/{}/web_static'.format(archive_folder))
+            c.run('rm -rf /data/web_static/current')
+            c.run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_folder))
 
-        # remove archive
-        run(f"rm /tmp/{filename}")
-
-        # move files into the right directory
-        run(f"mv {release_dir}/web_static/* {release_dir}")
-        run(f"rm -rf {release_dir}/web_static")
-
-        # delete symbolic link from web server
-        run("rm -rf /data/web_static/current")
-
-        # create a symlink to new version of code
-        run(f"ln -s {release_dir} /data/web_static/current")
-        return True
-    except:
-        return False
+    return True
