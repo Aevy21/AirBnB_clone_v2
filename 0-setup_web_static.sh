5a03135f277268cfc3cbd292658295b90eb66c09
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-03-07 13:10:26 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -36,6 +36,9 @@ echo "<html>
 # Create or recreate symbolic link
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
+# Ensure ownership of the symbolic link
+sudo chown -h ubuntu:ubuntu /data/web_static/current
+
 # Update Nginx configuration
 cat << EOF | sudo tee /etc/nginx/sites-available/default >/dev/null
 server {
