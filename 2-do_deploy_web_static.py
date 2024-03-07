b8258aceb62f0e2cc16bea18269439bf17e1d1e0
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-03-07 14:10:31 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -58,19 +58,19 @@ def do_deploy(archive_path):
         print("Archive doesn't exist.")
         return False
 
-    archive_filename = archive_path.split('/')[-1]
-    archive_folder = archive_filename.replace('.tgz', '')
+    archive_filename = archive_path.split("/")[-1]
+    archive_folder = archive_filename.replace(".tgz", "")
 
     for host in env.hosts:
         with Connection(host) as c:
-            c.put(archive_path, '/tmp')
+            c.put(archive_path, "/tmp")
 
-            c.run('mkdir -p /data/web_static/releases/{}'.format(archive_folder))
-            c.run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_filename, archive_folder))
-            c.run('rm /tmp/{}'.format(archive_filename))
-            c.run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_folder, archive_folder))
-            c.run('rm -rf /data/web_static/releases/{}/web_static'.format(archive_folder))
-            c.run('rm -rf /data/web_static/current')
-            c.run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_folder))
+            c.run("mkdir -p /data/web_static/releases/{}".format(archive_folder))
+            c.run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_filename, archive_folder))
+            c.run("rm /tmp/{}".format(archive_filename))
+            c.run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(archive_folder, archive_folder))
+            c.run("rm -rf /data/web_static/releases/{}/web_static".format(archive_folder))
+            c.run("rm -rf /data/web_static/current")
+            c.run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive_folder))
 
     return True
