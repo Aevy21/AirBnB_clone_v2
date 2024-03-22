527c58c0e03d2591e814c24e068fa83b44c49366
Avumile <Smithavery435@gmail.com>
2024-03-22 13:44:17 -0700

    pycodestye

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -9,16 +9,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Route 1: Display "Hello HBNB!"
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
 # Route 2: Display "HBNB"
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
 # Route 3: Display "C " followed by the value of the text variable
+
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     # Replace underscore symbols (_) with a space
@@ -26,6 +32,8 @@ def c_text(text):
     return f'C {text_with_spaces}'
 
 # Route 4: Display "Python " followed by the value of the text variable
+
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is_cool'):
@@ -34,17 +42,22 @@ def python_text(text='is_cool'):
     return f'Python {text_with_spaces}'
 
 # Route 5: Display "n is a number" if n is an integer
+
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
     return f'{n} is a number'
 
 # Route 6: Display HTML page with "Number: n" if n is an integer
+
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     if isinstance(n, int):
         return render_template('number_template.html', number=n)
     else:
         return 'Not a valid integer'
+
 
 if __name__ == '__main__':
     # Run the app on 0.0.0.0, port 5000
