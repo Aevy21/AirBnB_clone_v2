bb25e6b30ee76412dc9c8151e073ddcbc4e34cd6
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-03-24 23:30:32 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -19,7 +19,9 @@ class State(BaseModel, Base):
     else:
         @property
         def cities(self):
-            """Lists instances of City with state_id == to current State.id"""
-            from models import storage
-            return [city for city in storage.all(City).values()
-                    if city.state_id == self.id]
+            from models import storage  # Import locally here
+            cities_list = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+                    return cities_list
