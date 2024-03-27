84c692759e08ced5c19422c611af0d323e85f1dc
Avumile <Smithavery435@gmail.com>
2024-03-27 01:06:31 -0700

    fixing pycodestyle

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -35,6 +35,6 @@ def state_id(id):
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
