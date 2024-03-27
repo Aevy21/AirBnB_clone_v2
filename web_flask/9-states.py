a6311add3029269e6cb0ba3bf40086f70f9c3e74
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-03-27 13:33:16 +0200

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,44 +1,36 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application
+""" This module is a basic web app on Flask
+
+Listening port: 5000
 """
 
-
-from flask import Flask, render_template, abort
+from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.city import City
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """Closes the current SQLAlchemy Session"""
+def remove_session(exc):
+    """Removes the current SQLAlchemy session"""
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-def states_list():
-    """Displays a list of all State objects sorted by name"""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('9-states.html', states=sorted_states)
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays the list of states"""
+    all_states = storage.all("State").values()
+    return render_template("9-states.html", all_states=all_states)
 
 
-@app.route('/states/<id>', strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
 def state_id(id):
     """Displays dynamic HTML page for URI `/states/<id>`"""
-    state = storage.get(State, id)
-    if state is None:
-        return render_template("not_found.html")
-    
-    if storage.__class__.__name__ == 'DBStorage':
-        cities = sorted(state.cities, key=lambda city: city.name)
-    else:
-        cities = sorted(state.cities(), key=lambda city: city.name)
-    
-    return render_template("9-states.html", state=state, cities=cities)
+    all_states = storage.all("State").values()
+    for state in all_states:
+        if id == state.id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 if __name__ == "__main__":
