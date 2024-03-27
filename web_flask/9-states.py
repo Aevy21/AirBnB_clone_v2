4479a3c097c469f79e1afc6c7c4c5e082076bceb
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-03-27 12:43:12 +0200

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,7 +3,8 @@
 Starts a Flask web application
 """
 
-from flask import Flask, render_template
+
+from flask import Flask, render_template, abort
 from models import storage
 from models.state import State
 from models.city import City
@@ -22,17 +23,22 @@ def states_list():
     """Displays a list of all State objects sorted by name"""
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
-
     return render_template('states_list.html', states=sorted_states)
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def state_id(id):
     """Displays dynamic HTML page for URI `/states/<id>`"""
-    all_states = storage.all(State).values()
-    for state in all_states:
-        if id == state.id:
-            return render_template("9-states.html", state=state)
+    state = storage.get(State, id)
+    if state is None:
+        return render_template("not_found.html")
+    
+    if storage.__class__.__name__ == 'DBStorage':
+        cities = sorted(state.cities, key=lambda city: city.name)
+    else:
+        cities = sorted(state.cities(), key=lambda city: city.name)
+    
+    return render_template("state_cities.html", state=state, cities=cities)
 
 
 if __name__ == "__main__":
